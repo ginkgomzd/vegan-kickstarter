27e7f5adf84e2e3a9511982208f6a8ce00705b30
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-03-01 12:53:58 -0800

    Added ember app

---
+++ veganKickstart/app/router.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType
+});
+
+Router.map(function() {
+  this.route('day', {path: '/day/:index'});
+  this.route('recipe', {path: '/recipe/:index'});
+  this.route('login');
+  this.route('settings');
+});
+
+export default Router;
