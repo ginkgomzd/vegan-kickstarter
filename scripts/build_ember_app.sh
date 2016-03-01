5e864e671576f8c8c5c17d3851be377d978849b2
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-03-01 12:54:18 -0800

    Added ember build hook

---
+++ scripts/build_ember_app.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# http://lelandbatey.com/posts/2015/02/ember-and-cordova/ 
+
+if [[ $CORDOVA_CMDLINE =~ release ]]; then
+	echo "Creating production build of ember app"
+	(cd vegan* && ember build --environment=production)
+else
+	echo "Creating debug build of ember app"
+	(cd vegan* && ember build)
+fi
