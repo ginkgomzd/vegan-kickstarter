048059556b091aa045819f03e3a9db2816803675
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-03-02 15:18:52 -0800

    Created application route and connected it to appStartup functions

---
+++ veganKickstart/app/routes/application.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  setupUtils: Ember.inject.service('setup'),
+  model: function () {
+    return this.get("setupUtils").appStartup();
+  },
+  afterModel: function(transition) {
+    //This is where we will calculate which day should be shown
+  }
+});
