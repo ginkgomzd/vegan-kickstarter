fcc39a3721cb49e62d85dbd91563f68608ac0e9d
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-03-02 16:59:53 -0800

    Removed number as we are going to use ID instead

--- veganKickstart/app/models/day.js
+++ veganKickstart/app/models/day.js
@@ -1,7 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  number: DS.attr('number'),
   recipes: DS.hasMany('recipe'),
   messageTitle: DS.attr('string'),
   messageBody: DS.attr('string'),
