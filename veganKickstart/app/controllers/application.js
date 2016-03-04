438029bc8b7bd63ab62607bd2a0f67571a3f40ed
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-03-04 14:18:10 -0800

    Added platform specific styles

--- veganKickstart/app/controllers/application.js
+++ veganKickstart/app/controllers/application.js
@@ -2,6 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   hideBackButton: true,
+  init: function() {
+    if (window.cordova) {
+      Ember.$("body").addClass("platform-" + cordova.platformId);
+    } else {
+      //Default the styling to that of android
+      Ember.$("body").addClass("platform-android");
+
+      //This is for testing ios Specific styles and should be commented out for production
+      //Ember.$("body").addClass("platform-ios");
+    }
+  },
   actions: {
     executeBackAction: function() {
       window.history.back();
