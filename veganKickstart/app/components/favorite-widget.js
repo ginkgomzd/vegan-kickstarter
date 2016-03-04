be4d1924e9d455e414f888d78fa5224ef8ad038c
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-03-04 13:15:52 -0800

    Added favorite widget

---
+++ veganKickstart/app/components/favorite-widget.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'span',
+  classNames: ["glyphicon", "favorite", "right"],
+  classNameBindings: ["model.favorite:glyphicon-star:glyphicon-star-empty", "model.favorite:favorite-saved"],
+  click: function() {
+    this.get("model").toggleProperty("favorite");
+    this.get("model").save();
+  }
+});
