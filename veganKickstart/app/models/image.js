8851ca8c2508c6698c4eafc96d82e3da1d4c4ed2
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-03-03 18:52:03 -0800

    Renamed data column to imageDate because data is a reserved name in Ember

--- veganKickstart/app/models/image.js
+++ veganKickstart/app/models/image.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   mimeType: DS.attr('string'),
   path: DS.attr('string'),
-  data: DS.attr('string')
+  imageData: DS.attr('string')
 });
