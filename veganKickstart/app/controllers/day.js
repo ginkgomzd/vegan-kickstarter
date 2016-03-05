a9c0298f80a96a564e143628f2c3d807aa765c64
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-03-04 17:55:50 -0800

    Made all of recipe list item 'hot'

--- veganKickstart/app/controllers/day.js
+++ veganKickstart/app/controllers/day.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  showBackButton: 'never'
+  showBackButton: 'never',
+  actions: {
+    viewRecipe: function(recipe) {
+      this.transitionToRoute("recipe", recipe);
+    }
+  }
 });
