6e710e9e2a272983a7a460ddc6fc51eb74d96ff9
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-03-13 14:02:46 -0700

    Changed day view to be all days at once

--- veganKickstart/app/routes/day.js
+++ veganKickstart/app/routes/day.js
@@ -2,10 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function (params) {
-    if(params.index < 21) {
-      return this.get("store").find("day", params.index);
-    } else {
-      return {error: params.index};
-    }
+    return Ember.RSVP.hash({
+      "days": this.get("store").findAll("day"),
+      "firstSlide": parseInt(params.index) - 1
+    });
   }
 });
