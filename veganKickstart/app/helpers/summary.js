5987a2ea9c5e26d75ff436d8de2d11106a386079
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-03-13 20:48:28 -0700

    Added new summary helper to show a truncated string

---
+++ veganKickstart/app/helpers/summary.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Helper.helper(function(params) {
+  var summaryLength = params[1] || 300;
+  if(params[0].length > summaryLength) {
+    return params[0].substr(0, summaryLength) + "...";
+  } else {
+    return params[0];
+  }
+});
