66a362818cd6064e3bf2a1c39c7636ab48493cb9
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-03-14 10:51:16 -0700

    Added amazon sdk dependancy

--- veganKickstart/ember-cli-build.js
+++ veganKickstart/ember-cli-build.js
@@ -32,5 +32,7 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('bower_components/aws-sdk-js/dist/aws-sdk.min.js');
+
   return app.toTree();
 };
