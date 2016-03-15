7396e6990a8476000b7e805cf378668c1a11bb8c
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-03-14 23:01:52 -0700

    Added startup script that routes based on start Date

--- veganKickstart/app/routes/application.js
+++ veganKickstart/app/routes/application.js
@@ -3,12 +3,28 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   setupUtils: Ember.inject.service('setup'),
   settings: Ember.inject.service('settings'),
+  dateHelper: Ember.inject.service('date-functions'),
   model: function () {
     return this.get("setupUtils").appStartup();
   },
   afterModel: function(transition) {
     //This is where we will calculate which day should be shown
-    //this.transitionTo("day", 1);
+    var startedDateString = this.get("settings").load("startedKickstarter");
+    var day;
+
+    if (startedDateString) {
+      var started = new Date(startedDateString);
+      day = this.get("dateHelper").daysBetween(started);
+    } else {
+      this.get("settings").save("startedKickstarter", this.get("dateHelper").formatDate());
+      day = 1;
+    }
+
+    if ((day > 21) || day === "Invalid Date") {
+      this.transitionTo("day", 1);
+    } else {
+      this.transitionTo("day", day);
+    }
   },
   actions: {
     didTransition: function(transition) {
