cc2d44c1ebf51b9d3cc27df435752b3bff1ee17a
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-03-21 04:28:02 -0700

    Fixed error in summary helper when no data present

--- veganKickstart/app/helpers/summary.js
+++ veganKickstart/app/helpers/summary.js
@@ -2,9 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Helper.helper(function(params) {
   var summaryLength = params[1] || 300;
-  if(params[0].length > summaryLength) {
-    return params[0].substr(0, summaryLength) + "...";
+  if (params[0]) {
+    if (params[0].length > summaryLength) {
+      return params[0].substr(0, summaryLength) + "...";
+    } else {
+      return params[0];
+    }
   } else {
-    return params[0];
+    return "";
   }
 });
