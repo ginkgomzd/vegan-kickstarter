f3c574ee52f4342e445d584b451fb8791ba65b47
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-03-20 23:07:36 -0700

    Updated facebook service to fallback on Web service

--- veganKickstart/app/services/facebook.js
+++ veganKickstart/app/services/facebook.js
@@ -1,13 +1,32 @@
 import Ember from 'ember';
 
 var facebookService = Ember.Service.extend({
-  login: function() {
+  init: function() {
+    if (!this.canUseApp()) {
+      window.fbAsyncInit = function () {
+        FB.init({ // this line replaces FB.init({
+          appId: EmberENV.facebookAppID, // Facebook App ID
+          //status: false,  // check Facebook Login status
+          cookie: true,  // enable cookies to allow Parse to access the session
+          xfbml: false,  // initialize Facebook social plugins on the page
+          version: 'v2.3' // point to the latest Facebook Graph API version
+        });
+
+        // Run code after the Facebook SDK is loaded.
+      };
+
+      Ember.$('body').append(Ember.$("<div>").attr('id', 'fb-root'));
+      Ember.$.ajaxSetup({ cache: true });
+      Ember.$.getScript('https://connect.facebook.net/en_US/sdk.js');
+    }
+  },
+  appLogin: function() {
     var that = this;
     return new Ember.RSVP.Promise(function(resolve, reject) {
       if (facebookConnectPlugin) {
-        facebookConnectPlugin.login(["public_profile"], function (userData) {
-            that.loginSuccess(userData);
-            resolve(userData);
+        facebookConnectPlugin.login(["public_profile"], function (response) {
+            that.loginSuccess(response);
+            resolve(response);
           },
           reject
         );
@@ -15,6 +34,31 @@ var facebookService = Ember.Service.extend({
         reject("Unable to load Facebook Connect");
       }
     });
+  },
+  webLogin: function() {
+    var that = this;
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      if (typeof FB === "undefined") {
+        reject("Unable to load Facebook Connect");
+      } else {
+        FB.login(function (response) {
+          if (response.authResponse) {
+            that.loginSuccess(response);
+            resolve(response);
+          } else {
+            reject();
+          }
+        });
+
+      }
+    });
+  },
+  login: function() {
+    if (this.canUseApp()) {
+      return this.appLogin();
+    } else {
+      return this.webLogin();
+    }
   },
   loginSuccess: function(userData) {
     //todo: Connect to Cognito, or some other service.
@@ -41,13 +85,16 @@ var facebookService = Ember.Service.extend({
       }
     });
   },
+  canUseApp: function() {
+    return !(!navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/) || typeof facebookConnectPlugin === "undefined");
+  },
   loginStatus: function() {
     var that = this;
     return new Ember.RSVP.Promise(function(resolve, reject) {
-      if (facebookConnectPlugin) {
+      if (that.canUseApp()) {
         facebookConnectPlugin.getLoginStatus(resolve);
       } else {
-        reject("Unable to load Facebook Connect");
+        FB.getLoginStatus(resolve);
       }
     });
   }
