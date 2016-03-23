d1f4d25812b0fe529bcfedd43cdf36eab4b2098f
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-03-23 16:34:50 -0700

    Updated modal component to support a better list of params

--- veganKickstart/app/components/modal-window.js
+++ veganKickstart/app/components/modal-window.js
@@ -13,29 +13,57 @@ export default Ember.Component.extend({
   enabled: function() {
     return false;
   }.property(),
+  defaults: {
+    showClose: true,
+    clickToDismiss: false,
+    type: ""
+  },
   actions: {
     toggleModal: function() {
       this.toggleProperty('enabled');
     },
-    showModal: function(message, title, type, timeout) {
+    showModal: function(message, title, params) {
       this.set("message", message);
       this.set("title", title);
+
+      params = params || {};
+
+      var type = params.type || this.defaults.type;
       this.set("type", type);
-      this.set("enabled", true);
-      if (timeout) {
+
+      var clickToDismiss = params.clickToDismiss || this.defaults.clickToDismiss;
+      this.set("clickToDismiss", clickToDismiss);
+
+      var showClose = params.showClose || this.defaults.showClose;
+      this.set("showClose", showClose);
+
+      if (params.timeout) {
         var that = this;
-        Ember.run.later(function() {
+        Ember.run.later(function () {
           that.send("hideModal", false);
-        }, timeout);
+        }, params.timeout);
       }
+
+      this.set("enabled", true);
     },
     hideModal: function() {
       this.set("enabled", false);
+    },
+    clickToDismiss: function() {
+      if (this.get("clickToDismiss")) {
+        this.send("hideModal");
+      }
     }
   },
   expose: function() {
     var app_controller = this.get('targetObject');
     var exposedName = "comp-" + this.get('id');
     app_controller.set(exposedName, this);
+
+    //Set some defaults
+    this.defaults.clickToDismiss = this.get("clickToDismiss") || this.defaults.clickToDismiss;
+    this.defaults.type = this.get("type") || this.defaults.type;
+    this.defaults.showClose = this.get("showClose") || this.defaults.showClose;
+
   }.on('init')
 });
