b0c807a41507a367a3cdd216f1062a96c2f7ff43
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-03-23 11:15:08 -0700

    Updated image widget to add flag for conditionally showing the widget decoration

--- veganKickstart/app/components/image-widget.js
+++ veganKickstart/app/components/image-widget.js
@@ -3,6 +3,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'div',
   classNames: ['imageWidget'],
+  showDecoration: function() {
+    return (this.get("decoration") !== false);
+  }.property(),
   didInsertElement: function didInsertElement() {
     this.$(".slick-list").css("height", "auto");
   }
