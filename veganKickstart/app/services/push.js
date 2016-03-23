81ca70243022a56604f31aa76c4ab09ec9dbbfec
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-03-23 13:32:16 -0700

    Connected register device to new cognito functions.

--- veganKickstart/app/services/push.js
+++ veganKickstart/app/services/push.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
 
 var pushServices = Ember.Service.extend({
+  cognito: Ember.inject.service('cognito'),
   push: null,
   init: function() {
+    var pushService = this;
     if (typeof PushNotification !== "undefined") {
       //this is so that we can register multiple listeners.
       this.push = this.push || PushNotification.init({
@@ -18,9 +20,13 @@ var pushServices = Ember.Service.extend({
         });
 
       this.push.on('registration', function (data) {
-        console.log("Push Registration: ", data);
-        //todo: register this ID with Amazon.
-        // data.registrationId
+        //console.log("Push Registration: ", data);
+
+        //data.registrationId: The unique Device token to use in
+        //creation of a new application endpoint.
+
+        //Register this ID with Amazon.
+        pushService.get("cognito").registerDevice(data.registrationId);
       });
 
       this.push.on('error', function (e) {
@@ -34,14 +40,14 @@ var pushServices = Ember.Service.extend({
   register: function(context, notificationAction) {
     if (this.push) {
       this.push.on('notification', function (data) {
-        console.log("Push Notification: ", data);
-        context.send(notificationAction, data);
         // data.message,
         // data.title,
         // data.count,
         // data.sound,
         // data.image,
         // data.additionalData
+        //console.log("Push Notification: ", data);
+        context.send(notificationAction, data);
       });
     } else {
       console.log("No push service to register with");
