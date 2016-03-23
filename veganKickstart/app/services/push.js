5c7ead4643210163f26ad8b9c0191d52f3b3cc76
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-03-23 10:18:53 -0700

    Refactored init and registration

--- veganKickstart/app/services/push.js
+++ veganKickstart/app/services/push.js
@@ -2,30 +2,40 @@ import Ember from 'ember';
 
 var pushServices = Ember.Service.extend({
   push: null,
-  init: function(context, receivedAction) {
+  init: function() {
     if (typeof PushNotification !== "undefined") {
       //this is so that we can register multiple listeners.
-      this.push = this.push ||  PushNotification.init({
-        android: {
-          senderID: EmberENV.GCMSenderID
-        },
-        ios: {
-          alert: "true",
-          badge: "true",
-          sound: "true"
-        },
-        windows: {}
-      });
+      this.push = this.push || PushNotification.init({
+          android: {
+            senderID: EmberENV.GCMSenderID
+          },
+          ios: {
+            alert: "true",
+            badge: "true",
+            sound: "true"
+          },
+          windows: {}
+        });
 
       this.push.on('registration', function (data) {
-        console.log(data);
+        console.log("Push Registration: ", data);
         //todo: register this ID with Amazon.
         // data.registrationId
       });
 
+      this.push.on('error', function (e) {
+        console.log("Push Error: ", e);
+        // e.message
+      });
+    } else {
+      console.log("Push notifications Disabled");
+    }
+  },
+  register: function(context, notificationAction) {
+    if (this.push) {
       this.push.on('notification', function (data) {
-        console.log(data);
-        context.send(receivedAction, data);
+        console.log("Push Notification: ", data);
+        context.send(notificationAction, data);
         // data.message,
         // data.title,
         // data.count,
@@ -33,11 +43,8 @@ var pushServices = Ember.Service.extend({
         // data.image,
         // data.additionalData
       });
-
-      this.push.on('error', function (e) {
-        console.log(e);
-        // e.message
-      });
+    } else {
+      console.log("No push service to register with");
     }
   }
 });
