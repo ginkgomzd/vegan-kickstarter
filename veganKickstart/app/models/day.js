160db694ea5661d889707fa76b2f0c2916be60fb
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-03-23 10:27:16 -0700

    Changed day/image relationship 1:1

--- veganKickstart/app/models/day.js
+++ veganKickstart/app/models/day.js
@@ -4,5 +4,5 @@ export default DS.Model.extend({
   recipes: DS.hasMany('recipe'),
   messageTitle: DS.attr('string'),
   messageBody: DS.attr('string'),
-  images: DS.hasMany("image")
+  image: DS.belongsTo("image")
 });
