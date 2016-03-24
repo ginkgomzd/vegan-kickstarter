a4010c6f21815162dba65d5ee008583a9e703a3d
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-03-23 17:01:22 -0700

    Added ts service and global helper

--- veganKickstart/app/helpers/ts.js
+++ veganKickstart/app/helpers/ts.js
@@ -1,13 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Helper.helper(function(params) {
-  if(params.length > 1) {
-    return params[1];
-  } else {
-    if (params[0]) {
-      return params[0].charAt(0).toUpperCase() + params[0].slice(1);
-    } else {
-      return "";
-    }
-  }
+  return ts(params[0], params[1]);
 });
