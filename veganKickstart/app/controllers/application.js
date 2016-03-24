2e05b07dec684e3f87d9a0f15ba23867ac9396d4
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-03-23 17:46:43 -0700

    Update expected data structure of JSON and RAW notifications

--- veganKickstart/app/controllers/application.js
+++ veganKickstart/app/controllers/application.js
@@ -26,7 +26,11 @@ export default Ember.Controller.extend({
       this.transitionToRoute("search");
     },
     receivedPush: function(data) {
-      console.log("Application received push: ", data);
+      //console.log("Application received push: ", data);
+      var msg = data.message || data.additionalData.default || false;
+      if (msg) {
+        this.send("openModal", msg, data.title, data.additionalData);
+      }
     }
   }
 });
