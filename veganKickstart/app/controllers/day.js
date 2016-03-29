635f223a1c2d73e64683c5c2c5e48b9a3e1ed1bd
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-03-29 10:19:13 -0700

    Updated day screen to react to orientation changes, scroll properly and leave the title bar visible

--- veganKickstart/app/controllers/day.js
+++ veganKickstart/app/controllers/day.js
@@ -1,13 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend(Ember.Evented, {
+  orientationServices: Ember.inject.service('orientation'),
   showBackButton: 'never',
+  init: function() {
+    this.get("orientationServices").registerCallback("day", "resize", this);
+    return this._super();
+  },
+  willDestroy: function() {
+    this.get("orientationServices").removeCallback("day");
+    return this._super();
+  },
   firstSlide: function() {
     return this.get("model").firstSlide;
   }.property("model"),
 
   actions: {
     slickInit: function(obj) {
+      this.send("resize");
       Ember.$(obj).find(".slick-list").css({"height": "100%", "width": "100%"});
     },
     viewRecipe: function(recipe) {
@@ -22,12 +32,16 @@ export default Ember.Controller.extend(Ember.Evented, {
     },
     afterSlideChange: function() {
       if(this.changing) {
-        Ember.$('body,html').animate({scrollTop: 0}, 300);
+        Ember.$('#day-view').animate({scrollTop: 0}, 300);
         this.set("changing", false);
       }
     },
     reachedEdge: function(slick, direction) {
 
+    },
+    resize: function() {
+      var top = Ember.$(".main-nav").outerHeight();
+      Ember.$("#day-view").height(window.innerHeight - top);
     }
   }
 });
