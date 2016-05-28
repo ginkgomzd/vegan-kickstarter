0f7b7df6abd2ff316c8f07d0f5a0a7ca06c6a3ec
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-05-28 12:16:57 -0700

    Updated Share widget

--- veganKickstart/app/components/share-widget.js
+++ veganKickstart/app/components/share-widget.js
@@ -3,18 +3,34 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'span',
   classNames: ["glyphicon", "favorite", "right", "clickable", "glyphicon-share", "share-widget"],
-  classNameBindings: ["window.plugins.socialsharing:enabled:disabled"],
+  classNameBindings: ["enabled:enabled:disabled"],
+  enabled: function() {
+    return (window.plugins && window.plugins.socialsharing);
+  }.property(),
   click: function() {
     if (window.plugins && window.plugins.socialsharing) {
-      var options = {
-        message: model.url, // not supported on some apps (Facebook, Instagram)
-        subject: model.name, // fi. for email
-        files: [] // an array of filenames either locally or remotely
-        //url: 'https://www.website.com/foo/#bar?a=b',
-        //chooserTitle: 'Pick an app' // Android only, you can override the default share sheet title
-      };
 
-      window.plugins.socialsharing.shareWithOptions(options);
+      /*
+       * If you include the images Facebook assumes that is all you want to share
+       * Other share targets don't support live loading of meta-tags
+       * So for now we're just going to leave the images out.
+       */
+      /*
+      var images = [];
+      this.get("model").get("images").forEach(function(image) {
+        images.push("data:" + image.get("mimeType") + ";base64," + image.get("imageData"));
+      });
+      */
+
+      window.plugins.socialsharing.share(
+        //We're using jquery to strip out html tags here.
+        //We're adding the p tags in case the description doesn't have html
+        //Because that would cause jQuery to freak out.
+        Ember.$("<p>" + this.get("model").get("description") + "</p>").text(),
+        this.get("model").get("name"),
+        null, //images,
+        this.get("model").get("url")
+      );
     }
   }
 });
