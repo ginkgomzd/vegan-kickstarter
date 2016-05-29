5e8e25a0d394fbd6d0991382fd995e111f3a96b6
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-05-29 11:51:36 -0700

    Updated Image model for cache style storage

--- veganKickstart/app/models/image.js
+++ veganKickstart/app/models/image.js
@@ -2,6 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   mimeType: DS.attr('string'),
-  path: DS.attr('string'),
-  imageData: DS.attr('string')
+  remotePath: DS.attr('string')
 });
