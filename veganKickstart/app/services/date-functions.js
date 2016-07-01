855a91ed904664418b4f663dc71da9ceab5a4ec4
TobiasLounsbury <tobiaslounsbury@gmail.com>
2016-07-01 01:20:22 -0700

    Refactored date-functions to use slashes instead of dashes in dates because mobiles safari chokes when you use dashes

--- veganKickstart/app/services/date-functions.js
+++ veganKickstart/app/services/date-functions.js
@@ -2,32 +2,31 @@ import Ember from 'ember';
 
 var dateServices = Ember.Service.extend({
 
-  secondsPerDay: 8.64e7,
+  millisecondsPerDay: 86400000,
 
   formatDate: function(date) {
     date = date || new Date();
     //the month +1 is to account for javascript getMonth returning a zero-based index
-    return date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+    return date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate();
   },
 
   formatDateTime: function(date) {
     date = date || new Date();
     //the month +1 is to account for javascript getMonth returning a zero-based index
-    return date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes();
+    return date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes();
   },
 
   daysBetween: function(a, b) {
     b = b || new Date();
     a = new Date(this.formatDate(a));
     b = new Date(this.formatDate(b));
-
-    return Math.round(Math.abs(a.getTime() - b.getTime()) / this.secondsPerDay);
+    return Math.round(Math.abs(a.getTime() - b.getTime()) / this.millisecondsPerDay);
   },
 
   daysAgo: function(days, date) {
     var from = date || new Date();
     //This converts "days ago" to a timestring and into a Date object
-    return new Date(from.getTime() - (parseInt(days) * this.secondsPerDay));
+    return new Date(from.getTime() - (parseInt(days) * this.millisecondsPerDay));
   }
 
 
